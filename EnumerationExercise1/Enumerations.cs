218343cda35505c935f8430c11c7c1df0c3a0be4
Atte Collan <attcol102@keladev.fi>
2018-06-01 10:10:56 +0300

    testing

--- EnumerationExercise1/Enumerations.cs
+++ EnumerationExercise1/Enumerations.cs
@@ -3,13 +3,23 @@
 namespace EnumerationExercise1
 {
     enum Days { ma, ti, ke, to, pe, la, su };
-    
+
     enum Months { tammi, helmi, maalis, huhti, touko, kesä, heinä, elo, syys, loka, marras, joulu };
-    
+
     enum Alphabet { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, y, z, å, ä, ö }
 
     [Flags]
-    enum Colours { sininen, punainen, keltainen, vihreä, oranssi, pinkki, musta, valkoinen }
+    enum Colours
+    {
+        sininen = 0x0,
+        punainen = 0x1,
+        keltainen = 0x2,
+        vihreä = 0x4,
+        oranssi = 0x8,
+        pinkki = 0x10,
+        musta = 0x20,
+        valkoinen = 0x40
+    }
 
 }
 
--- EnumerationExercise1/Program.cs
+++ EnumerationExercise1/Program.cs
@@ -11,8 +11,11 @@ namespace EnumerationExercise1
         //some changes more to test git push
         static void Main(string[] args)
         {
+            Console.WriteLine("väriyhdistelmät:");
+            for (int val = 0; val <= 16; val++)
+                Console.WriteLine("{0,3} - {1:G}", val, (Colours)val);
 
-        
+            Console.ReadLine();
         
         }
     }
